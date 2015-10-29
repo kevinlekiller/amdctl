0dc2c7a95a1b2de89e2d69af218a6dc31180b438
kevinlekiller <kevinlekiller@gmail.com>
2015-10-29 15:49:51 -0400

    Show power in watts.

--- amdctl.c
+++ amdctl.c
@@ -318,7 +318,7 @@ void printBaseFmt(const int idd) {
 	
 	if (idd) {
 		int IddDiv = getDec(IDD_DIV_BITS);
-		printf("\t\tCPU current divisor id  %d\n", IddDiv);
+		printf("\t\tCore current divisor id  %d\n", IddDiv);
 		switch (IddDiv) {
 			case 0b00:
 				IddDiv = 1;
@@ -335,9 +335,9 @@ void printBaseFmt(const int idd) {
 		}
 		int cpuCurrent = getDec(IDD_VALUE_BITS);
 		int cpuCurrDraw = abs(cpuCurrent / IddDiv);
-		printf("\t\tCPU current id          %d\n", cpuCurrent);
-		printf("\t\tCPU current draw        %dA\n", cpuCurrDraw);
-		printf("\t\tCPU power draw          %2.fmW\n", (cpuCurrDraw * cpuVolt));
+		printf("\t\tCore current id          %d\n", cpuCurrent);
+		printf("\t\tCore current draw        %dA\n", cpuCurrDraw);
+		printf("\t\tCore power draw         %.2fW\n", ((cpuCurrDraw * cpuVolt) / 1000));
 	}
 }
 
